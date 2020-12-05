901878945fa461bd36859b56bf8b808b26db5436
HugoUnosson <75093627+HugoUnosson@users.noreply.github.com>
2020-12-05 15:18:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    Console.Write(input[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
